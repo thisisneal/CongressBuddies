1cff82bb2edb004fd094fe061cdda0aa3e434067
Vijay Thurimella <vt@cmu.edu>
2013-08-03 22:50:10 -0700

    Fixed bugs

--- getPhoto.sh
+++ getPhoto.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [ ${#@} -eq 1 ]; then
-    echo http://www.govtrack.us/data/photos/$1.jpeg
+    ID=$1
+    if [[ 300001 -le $ID && $ID -le 412585 ]]; then 
+        wget http://www.govtrack.us/data/photos/${ID}.jpeg -O ${ID}.jpeg
+    else
+        echo "Not in range"
+    fi
 else
     echo "Usage: ./getPhotos.sh [idnumber]"
 fi
