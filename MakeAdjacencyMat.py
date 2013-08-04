30025f5ce41b822a697289b0950c8c7a382c5aa0
Neal Bhasin <neal@cmu.edu>
2013-08-03 21:34:59 -0700

    abstractions n stuff

--- MakeAdjacencyMat.py
+++ MakeAdjacencyMat.py
@@ -4,6 +4,19 @@ import json
 pathToVoteDirs = "votes/2013/"
 prefix = "h"
 
+# Increment the similarity count between two people
+def incrementSimilarity(perA, perB):
+    pass
+
+# Given an array of individual votes that matched vote type and bill,
+# increment the similarity counts in adjaceny matrix between all people involved
+def countSamevotes(votes):
+    for vote in votes:
+        cur_person = vote['id']
+        for vote in votes:
+            other_person = vote['id']
+        incrementSimilarity(cur_person, other_person)
+
 def fooFile(jsonFile):
     json_data = open(jsonFile).read()
     data = json.loads(json_data)
@@ -12,7 +25,8 @@ def fooFile(jsonFile):
         pres = votes['Present']
         ayes = votes['Yea']
         nays = votes['Nay']
-        print json.dumps(ayes)
+        countSameVotes(ayes)
+        countSameVotes(nays)
     except:
         pass
 
