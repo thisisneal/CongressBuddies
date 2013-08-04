44566bce24d0de573f8b3ce3cfcebadc6ae18018
Neal Bhasin <neal@cmu.edu>
2013-08-03 21:10:05 -0700

    start json parsin

--- MakeAdjacencyMat.py
+++ MakeAdjacencyMat.py
@@ -2,12 +2,21 @@ import os
 import json
 
 pathToVoteDirs = "votes/2013/"
+prefix = "h"
 
 def fooFile(jsonFile):
-    json_data=open(jsonFile).read()
+    json_data = open(jsonFile).read()
     data = json.loads(json_data)
-    print(data)
+    try:
+        votes = data['votes']
+        pres = votes['Present']
+        ayes = votes['Yea']
+        nays = votes['Nay']
+        print json.dumps(ayes)
+    except:
+        pass
 
 for dirname, dirnames, filenames in os.walk(pathToVoteDirs):
     for subdirname in dirnames:
-        fooFile(pathToVoteDirs + subdirname + "/data.json")
+        if subdirname.startswith(prefix):
+            fooFile(pathToVoteDirs + subdirname + "/data.json")
