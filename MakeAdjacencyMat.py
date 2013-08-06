18af0f7ddfeeea9190e16d5a3df7bc9192a9b635
Vijay Thurimella <vt@cmu.edu>
2013-08-05 18:35:07 -0700

    ld

--- MakeAdjacencyMat.py
+++ MakeAdjacencyMat.py
@@ -9,6 +9,22 @@ adjacenyMap = {} # Person onto {Person onto closeness count}
 bioMap = {}  # ID onto bio data
 nameMap = {} # Name onto ID
 
+def min(intA, intB):
+    if(intA < intB): return intA
+    return intB
+
+def dist(strA, strB):
+    lenA = len(strA)
+    lenB = len(strA)
+
+    if(len(strA) == 0): return lenB
+    if(len(strB) == 0): return lenA
+
+    cost = 0
+    if (s[len_s-1] != t[len_t-1]): cost = 1;
+
+    return min(min(dist(strA[0:lenA-1], strB)+1, dist(strA, strB[0:lenB-1])+1), dist(strA[0:lenA-1], strB[0:lenB-1])+cost)
+
 # Increment the similarity count between two people
 def incrementSimilarityBi(perA, perB):
     incrementSimilarity(perA, perB)
